ac5b427b184edee6457d945c159e25f25e044b3a
vinayvishwkarma <vinaycse0114@gmail.com>
2021-02-02 15:40:44 +0200

    Set logger in the app

--- src/main/java/com/finago/interview/task/util/Test.java
+++ src/main/java/com/finago/interview/task/util/Test.java
@@ -1,28 +1,25 @@
 package com.finago.interview.task.util;
 
-import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
  
 public class Test {
 	
+///batch-processor/resources/log4j.properties
+ /*static Logger logger = Logger.getLogger(Test.class);
+ static final String path = "resources/log4j.properties";*/
+	
+ 	static Logger logger = Logger.getLogger(Test.class);
 
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+		
  
-
-	/*public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-		
-		//"/batch-processor/data/in copy/110011003.pdf"
-		File f = new File("data/in copy/110011003.pdf");
-		
-		File f1 = new File("data/in");
-		System.out.println(f.exists() && !f.isDirectory()) ;
-		System.out.println(!f1.exists() && f1.isDirectory()) ;
-
         logger.info("logger is working ");
 
  
-	}*/
+	}
 }
